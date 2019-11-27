5de2863067255c212ed1528700ec798b4bb4f158
Sohel Kabir <33397287+SohelKabir@users.noreply.github.com>
2019-11-28 00:18:02 +0600

    first commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation from North South University<span> 2015  - 2019</span></h2>
+                        <p>I have completed my BSc in Computer Science and Engineering from North South University in September'19. </p>
+                        <p>During my undergread life I have studied Fundamentals of Computer Science, Machine Learning, Computer Networking, Computer Vision, Natural Language Processing(NLP), Data Science,  Web Development and Internet Technology etc.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      <h2>HSC (Higher Secondary) <span>2012-2014</span></h2>
+                        <p>I have completed my higher secondary education from Chattogram Cantonment public College with Science major  </p> 
+                      </div>  
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
