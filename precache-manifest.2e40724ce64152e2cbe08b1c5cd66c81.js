62451be82455c523792a50434ecb3487d8f905b0
imsohelkabir <nurul@ihealthscreen.org>
2021-09-21 23:15:50 +0600

    Updates

---
+++ precache-manifest.2e40724ce64152e2cbe08b1c5cd66c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb4449a71c8af9e8dfa",
+    "url": "/sohelkabir.github.io/static/css/main.e3f7f0db.chunk.css"
+  },
+  {
+    "revision": "2eb4449a71c8af9e8dfa",
+    "url": "/sohelkabir.github.io/static/js/main.2eb4449a.chunk.js"
+  },
+  {
+    "revision": "b64608eea45911b7df70",
+    "url": "/sohelkabir.github.io/static/js/runtime~main.b64608ee.js"
+  },
+  {
+    "revision": "77e2a8e6f10b7a79c9ca",
+    "url": "/sohelkabir.github.io/static/js/2.77e2a8e6.chunk.js"
+  },
+  {
+    "revision": "dc685158156d2d0f71ac3552ab78d8c7",
+    "url": "/sohelkabir.github.io/index.html"
+  }
+];
