d112e08146351b75077222016fc1034e63525d16
imsohelkabir <nurul@ihealthscreen.org>
2021-09-22 02:14:42 +0600

    updated site

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,74 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
-                    <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>Hi! I'm Mohammad Nurul Kabir Sohel, recently I have graduated from North South University from department of Electrical and Computer Engineering. </p>
-                    <p>Right now I'm looking forward to build my career in a professional organization where I can learn and develop my skills to be ready for the future challenges.</p>
+        <section className='colorlib-about' data-section='about'>
+          <div className='colorlib-narrow-content'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div
+                  className='row row-bottom-padded-sm animate-box'
+                  data-animate-effect='fadeInLeft'
+                >
+                  <div className='col-md-12'>
+                    <div className='about-desc'>
+                      <span className='heading-meta'>About Us</span>
+                      <h2 className='colorlib-heading'>Who Am I?</h2>
+                      <p>
+                        Hi! I'm Mohammad Nurul Kabir Sohel, recently I have
+                        graduated from North South University from department of
+                        Electrical and Computer Engineering.{' '}
+                      </p>
+                      <p>
+                        Currently I'm working in software development firm based
+                        in Dhaka as a Software Engineer.
+                      </p>
                     </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </section>
-        <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+        <section className='colorlib-about'>
+          <div className='colorlib-narrow-content'>
+            <div className='row'>
+              <div
+                className='col-md-6 col-md-offset-3 col-md-pull-3 animate-box'
+                data-animate-effect='fadeInLeft'
+              >
+                <span className='heading-meta'>What I do?</span>
+                <h2 className='colorlib-heading'>
+                  Here are some of my expertise
+                </h2>
+              </div>
             </div>
-            </div>
-            <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-1">
-                <span className="icon">
-                    <i className="icon-bulb" />
-                </span>
-                <div className="desc">
+            <div className='row row-pt-md'>
+              <div className='col-md-4 text-center animate-box'>
+                <div className='services color-1'>
+                  <span className='icon'>
+                    <i className='icon-bulb' />
+                  </span>
+                  <div className='desc'>
                     <h3>Web Development </h3>
-                    <p>I have experience working with Javascript, HTML, CSS, SCSS, React, NodeJS, Flask, Docker, Spring MVC ect.</p>
+                    <p>
+                      I have experience working with web technologies like
+                      Node.js, Express.js, React, webRTC, Redux, Jest, Flask,
+                      Babel, Webpack, GraphQL, PostgreSQL, MySQL,MS SQL, Oracle
+                      DB, MongoDB, Firebase etc .
+                    </p>
+                  </div>
                 </div>
+              </div>
+              <div className='col-md-4 text-center animate-box'>
+                <div className='services color-3'>
+                  <span className='icon'>
+                    <i className='icon-phone3' />
+                  </span>
+                  <div className='desc'>
+                    <h3>Mobile Development</h3>
+                    <p>
+                      I have developed and deployed in hybrid mobile development
+                      technologies like React Native and Flutter. Also worked
+                      with backend/server SASS like Firebase, AWS, Atlas, GCP
+                      etc.
+                    </p>
+                  </div>
                 </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-3">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>Since I'm a CSE graduate I have a good understanding on Data Structures and Algorithms</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-5">
-                <span className="icon">
-                    <i className="icon-data" />
-                </span>
-                <div className="desc">
+              </div>
+              <div className='col-md-4 text-center animate-box'>
+                <div className='services color-5'>
+                  <span className='icon'>
+                    <i className='icon-data' />
+                  </span>
+                  <div className='desc'>
                     <h3>Machine Learning</h3>
-                    <p>I have studied classical Machine Learning, Deep Learning, Computer Vision and AI at my University</p>
+                    <p>
+                      I have studied classical Machine Learning, Deep Learning,
+                      Computer Vision and AI at my University. Frameworks/Tools:
+                      Scikit-Learn, NumPy , SciPy, Matplotlib, Pandas, NLTK,
+                      Anaconda, OpenCV, Seaborn, Plotly.
+                    </p>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-           
-            </div>
-        </div>
+          </div>
         </section>
       </div>
-    )
+    );
   }
 }
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -12,7 +12,125 @@ export default class Timeline extends Component {
                 data-animate-effect='fadeInLeft'
               >
                 <span className='heading-meta'>highlights</span>
-                <h2 className='colorlib-heading animate-box'>Timeline</h2>
+                <h2 className='colorlib-heading animate-box'>
+                  Timeline (Professional Experiences)
+                </h2>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='timeline-centered'>
+                  <article
+                    className='timeline-entry animate-box'
+                    data-animate-effect='fadeInLeft'
+                  >
+                    <div className='timeline-entry-inner'></div>
+                  </article>
+                  <article
+                    className='timeline-entry animate-box'
+                    data-animate-effect='fadeInTop'
+                  >
+                    <div className='timeline-entry-inner'>
+                      <div className='timeline-icon color-4'>
+                        <i className='icon-briefcase3' />
+                      </div>
+                      <div className='timeline-label'>
+                        <h2>
+                          Software Engineer,
+                          <span> Nanoid Limited, Dhaka, May '21 - Present</span>
+                        </h2>
+                        <p>
+                          Working as Full Stack Software Engineer in Nanoid
+                          Limited. We are building amazing mobile and web apps
+                          for small business all over the country. Mobile app is
+                          online at{' '}
+                          <a href='https://play.google.com/store/apps/details?id=com.halkhata'>
+                            Google Play Store
+                          </a>
+                          .
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className='timeline-entry animate-box'
+                    data-animate-effect='fadeInLeft'
+                  >
+                    <div className='timeline-entry-inner'>
+                      <div className='timeline-icon color-5'>
+                        <i className='icon-briefcase3' />
+                      </div>
+                      <div className='timeline-label'>
+                        <h2>
+                          Software Engineer{' '}
+                          <span>
+                            IhealthScreen Bangladesh, Dhaka, Aug '20 - Apr '21
+                          </span>
+                        </h2>
+                        <p>
+                          Developed telemedicine mobile applications with
+                          realtime audio, video and chat services using
+                          technology stacks React
+                          Native,React-native-webRTC,Redux/Redux-Toolkit,React
+                          native elements, Native base for frontend and
+                          Node/ExpressJS, Socket.io, MongoDB for backend and
+                          database management. Live at{' '}
+                          <a href='https://play.google.com/store/apps/details?id=com.ihs.myhealth'>
+                            Google Play Store
+                          </a>{' '}
+                          and{' '}
+                          <a href='https://apps.apple.com/app/id1551584082'>
+                            Apple App Store
+                          </a>{' '}
+                          .
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className='timeline-entry animate-box'
+                    data-animate-effect='fadeInLeft'
+                  >
+                    <div className='timeline-entry-inner'>
+                      <div className='timeline-icon color-5'>
+                        <i className='icon-briefcase3' />
+                      </div>
+                      <div className='timeline-label'>
+                        <h2>
+                          Junior Software Engineer{' '}
+                          <span>
+                            Star Computer Systems Limited, Dhaka, Jan '20 - July
+                            '21
+                          </span>
+                        </h2>
+                        <p>
+                          Developed ERP softwares using technology stack React,
+                          Redux, Material-UI, Reactstrap, Bit.dev etcetera for
+                          front-end and .NET Core, MS SQL server for back-end
+                          and database management.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className='timeline-entry begin animate-box'
+                    data-animate-effect='fadeInBottom'
+                  >
+                    <div className='timeline-entry-inner'>
+                      <div className='timeline-icon color-none'></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div
+                className='col-md-6 col-md-offset-3 col-md-pull-3 animate-box'
+                data-animate-effect='fadeInLeft'
+              >
+                <h2 className='colorlib-heading animate-box'>
+                  Timeline (Education)
+                </h2>
               </div>
             </div>
             <div className='row'>
